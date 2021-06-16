a8e8fd537fb048562db6316681d7342323d3df11
guayana <isabelaguayanaz@gmail.com>
2021-06-16 17:54:33 -0300

    estilização do search

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,6 +1,16 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-    background-color: ${(props) => props.theme.colors.primary};
+export const Container = styled.aside`
+    background-color: ${(props) => props.theme.colors.background};
+    width: 350px;
+    height: 100vh;
+    overflow-y: auto;
+`
 
+export const Search = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: #fff;
+    padding: 16px;
 `
