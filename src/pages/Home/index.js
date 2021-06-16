caed9d8b5136608e4855a66db3d6e915eb086a99
guayana <isabelaguayanaz@gmail.com>
2021-06-16 18:18:59 -0300

    envelopamneto do wrapper e adição do icon search

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
 import TextField, { Input } from '@material/react-text-field'
-import { Container, Search } from './styled'
+import { Container, Search, Logo, Wrapper } from './styled'
+import MaterialIcon from '@material/react-material-icon'
 
 //TO DO COLOCAR UMA IMAGEM PRA LOGO
 const Home = () => {
     const [inputValue, setInputValue] = useState('')
     console.log(inputValue)
     return(
-        <Container>
-            <Search>
-                <h1>LOGO</h1>
-                <TextField label="pesquisar" outlined> 
-                    <Input 
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
-                    />
-                </TextField>
-            </Search>
-        </Container>
+        <Wrapper>
+            <Container>
+                <Search>
+                    <Logo>LOGO</Logo>
+                    <TextField 
+                        label="pesquisar" 
+                        outlined
+                        trailingIcon={<MaterialIcon role="button" icon="search"/>}
+                        > 
+                        <Input 
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                        />
+                    </TextField>
+                </Search>
+            </Container>
+        </Wrapper>
     )
 }
 
